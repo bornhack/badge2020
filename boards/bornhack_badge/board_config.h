b65912e7032a85202a654e59c064f273fa93cff5
Thomas Flummer <tf@flummer.net>
2020-08-16 12:02:09 +0200

    Added BornHack 2020 Badge
    Preliminary version

---
+++ boards/bornhack_badge/board_config.h
@@ -0,0 +1,17 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "bornhack"
+#define PRODUCT_NAME "Bornhack 2020 Badge"
+#define VOLUME_LABEL "BHBADGEBOOT"
+#define INDEX_URL "https://github.com/bornhack/badge2020/"
+#define BOARD_ID "SAMD21G18A-BornHackBadge"
+
+#define USB_VID 0x239A
+#define USB_PID 0x0057
+
+#define LED_PIN PIN_PB08
+
+#endif
